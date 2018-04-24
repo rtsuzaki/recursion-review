c92c93ccca3a7f38af22ff48a72e109f6199a409
rtsuzaki <rtsuzaki10@gmail.com>
2018-04-23 21:22:34 -0700

    Made some additions to parseJSON for parsing objects

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -16,15 +16,15 @@ if (json === undefined || typeof json === 'function') {
     return false;
 } else if (json === 'null'){
     return null;
-} else if (typeof json === 'string' && json[0] === '[' && json[json.length-1] === ']'){
+} else if (typeof json === 'string' && json[0] === '[' && json[json.length-1] === ']') {
     if (json.length === 2) {
       return [];
     } else {
       var result = [];
     //'["one", "two"]'
-      var noSpaces = json.split(' ').join('')
+      var noSpaces = json.split(' ').join('');
     //'["one","two"]'
-      console.log(noSpaces)
+      console.log(noSpaces);
       var slice1 = noSpaces.slice(1, noSpaces.indexOf(']'));
       var array = slice1.split(',')
       console.log(array)
@@ -34,12 +34,29 @@ if (json === undefined || typeof json === 'function') {
         };
       return result;
     } 
-} else if (json.match(/\d/g)){
+} else if (json.match(/\d/g)){ 
     return Number(json);
+} else if (typeof json === 'string' && json[0] === '{' && json[json.length-1] === '}') {
+    if (json.length === 2) {
+      return {};
+    } else {
+      //'{"a": "b", "c": "d"}'
+       var noSpaces = json.split(' ').join('');
+      //'{"a":"b","c":"d"}'
+       console.log(noSpaces);
+       var noBraces = noSpaces.slice(1,noSpaces.length-1);
+       console.log(noBraces);
+      //["{"a":"b"", ""c":"d"}"]
+       var key = noSpaces.slice(2, noSpaces.indexOf(':')-1);
+       var val = noSpaces.slice(noSpaces.indexOf(':') + 2, noSpaces.length-2);
+       var result = {};
+       result[key] = val;
+       return result; 
+    }
 } else if (typeof json === 'string') {
       return json.slice(1,json.length-1);
   }
-}  
+} 
 
 
 
